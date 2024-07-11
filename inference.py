b857106d3e93b32973401d0ff5fc33e357ee3cb0
alexander paskal <alexcpaskal@gmail.com>
2024-07-10 23:26:09 -0700

    working inference

--- inference.py
+++ inference.py
@@ -1,8 +1,44 @@
-from ray.rllib.core.rl_module.rl_module import RLModule
-import os
+import ray
+from ray.rllib.algorithms import Algorithm
+from ray.tune.registry import register_env
+import gym
+import numpy as np
 
-rl_module = RLModule.from_checkpoint(
-    "C:/Users/alexc/ray_results/PPO_2024-07-07_16-46-55/PPO_Env_0c24f_00000_0_2024-07-07_16-46-55/checkpoint_000000/policies/default_policy"
-)
+
+from env import Env
+
+
+# Register the custom environment
+register_env("CustomEnv-v0", lambda config: Env(config))
+
+# Initialize Ray with minimal resources
+ray.init(num_cpus=1, num_gpus=0)
+
+# Load the trained model
+checkpoint_path = "C:/Users/alexc/ray_results/PPO_2024-07-08_06-05-24/PPO_Env_bd224_00000_0_2024-07-08_06-05-24/checkpoint_000000"
+loaded_algo = Algorithm.from_checkpoint(checkpoint_path)
+
+# get model
+policy = loaded_algo.get_policy()
+breakpoint()
+model = policy.model
+# Set up the environment for inference
+env = Env()
+
+# Perform inference
+num_episodes = 10
+for episode in range(num_episodes):
+    obs, *_ = env.reset()
+    done = False
+    episode_reward = 0
+    while not done:
+        action = loaded_algo.compute_single_action(obs)
+        obs, reward, done, *_ = env.step(action)
+        episode_reward += reward
+    print(f"Episode {episode + 1} reward: {episode_reward}")
+
+# Shut down Ray
+ray.shutdown()
+
 
 print()
